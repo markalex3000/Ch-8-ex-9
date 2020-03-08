7dc35cdaf1c7d648b9c67b15384501b00b3cf63a
Mark Alexieff <mark.alexieff@outlook.com>
2020-03-07 23:55:02 -0800

    First effort, needs some cleaning up.

--- Ch 8 ex 9/Source.cpp
+++ Ch 8 ex 9/Source.cpp
@@ -1,4 +1,6 @@
 /*  Program Description
+Write a function that given two vector<double>s price and weight computes a value (an “index
+that is the sum of all price[i]*weight[i]. Make sure to have weight.size()==price.size().
 */
 
 /*
@@ -11,12 +13,16 @@ Standard opening
 global variables
 */
 
+vector<double> price;
+vector<double> weight;
+
 
 
 /*
 functions declarations
 */
 
+double calc_index(vector<double>& p, vector<double>& w);
 
 
 /*
@@ -24,7 +30,27 @@ main
 */
 
 int main() {
+	double temp_price{ 0 };
+	double temp_weight{ 0 };
+	int count{ 0 };
+	cout << "Enter a series of prices (999 to exit):";
+	cin >> temp_price;
+	while (temp_price != 999) {
+		count++;
+		price.push_back(temp_price);
+		cin >> temp_price;
+	}
 
+	cout << "\n" << "Enter a weight for each price...";
+	for (double p : price) {
+		cout << "Weight for price " << p << "is? > ";
+		cin >> temp_weight;
+		weight.push_back(temp_weight);
+		cout << "\n";
+	}
+
+	cout << "The index = " << calc_index(price, weight);
+	keep_window_open();
 }
 
 
@@ -32,4 +58,12 @@ int main() {
 funcion definitions
 */
 
+double calc_index(vector<double>& p, vector<double>& w) {
+	auto w_itr = w.begin();
+	double index{ 0 };
+	for (double p : p) {
+		index += p * *w_itr;
+	}
+	return index;
+}
 
