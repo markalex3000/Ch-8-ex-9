b5ed207618cf292a5b4f7f49e325312da63c1c4d
Mark Alexieff <mark.alexieff@outlook.com>
2020-03-07 23:36:53 -0800

    Add project files.

---
+++ Ch 8 ex 9/Source.cpp
@@ -0,0 +1,35 @@
+/*  Program Description
+*/
+
+/*
+Standard opening
+*/
+
+#include "C:\Users\mark.alexieff\source\repos\std_lib_facilities.h"
+
+/*
+global variables
+*/
+
+
+
+/*
+functions declarations
+*/
+
+
+
+/*
+main
+*/
+
+int main() {
+
+}
+
+
+/*
+funcion definitions
+*/
+
+
